dac108b2328a1cbf94403b8ce49a14291f2fcdcd
Young Chan Han <hanyc1205@gmail.com>
2025-05-09 10:03:09 -0400

    worked for sign in funcitonality

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
-    template: `$s, | Prostore`,
+    template: `%s | Prostore`,
     default: APP_NAME,
   },
   description: APP_DESCRIPTION,

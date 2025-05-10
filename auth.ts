f8a23afd89179464b2b197e109e265e1f15fcb92
Young Chan Han <hanyc1205@gmail.com>
2025-05-10 07:56:36 -0400

    added jwt for sign up

--- auth.ts
+++ auth.ts
@@ -70,55 +70,56 @@ export const config = {
 
       return session;
     },
-    // async jwt({ token, user, trigger, session }: any) {
-    //   // Assign user fields to token
-    //   if (user) {
-    //     token.id = user.id;
-    //     token.role = user.role;
 
-    //     // If user has no name then use the email
-    //     if (user.name === "NO_NAME") {
-    //       token.name = user.email!.split("@")[0];
+    async jwt({ token, user, trigger, session }: any) {
+      // Assign user fields to token
+      if (user) {
+        token.id = user.id;
+        token.role = user.role;
 
-    //       // Update database to reflect the token name
-    //       await prisma.user.update({
-    //         where: { id: user.id },
-    //         data: { name: token.name },
-    //       });
-    //     }
+        // If user has no name then use the email
+        if (user.name === "NO_NAME") {
+          token.name = user.email!.split("@")[0];
 
-    //     if (trigger === "signIn" || trigger === "signUp") {
-    //       const cookiesObject = await cookies();
-    //       const sessionCartId = cookiesObject.get("sessionCartId")?.value;
+          // Update database to reflect the token name
+          await prisma.user.update({
+            where: { id: user.id },
+            data: { name: token.name },
+          });
+        }
 
-    //       if (sessionCartId) {
-    //         const sessionCart = await prisma.cart.findFirst({
-    //           where: { sessionCartId },
-    //         });
+        // if (trigger === "signIn" || trigger === "signUp") {
+        //   const cookiesObject = await cookies();
+        //   const sessionCartId = cookiesObject.get("sessionCartId")?.value;
 
-    //         if (sessionCart) {
-    //           // Delete current user cart
-    //           await prisma.cart.deleteMany({
-    //             where: { userId: user.id },
-    //           });
+        //   if (sessionCartId) {
+        //     const sessionCart = await prisma.cart.findFirst({
+        //       where: { sessionCartId },
+        //     });
 
-    //           // Assign new cart
-    //           await prisma.cart.update({
-    //             where: { id: sessionCart.id },
-    //             data: { userId: user.id },
-    //           });
-    //         }
-    //       }
-    //     }
-    //   }
+        //     if (sessionCart) {
+        //       // Delete current user cart
+        //       await prisma.cart.deleteMany({
+        //         where: { userId: user.id },
+        //       });
 
-    //   // Handle session updates
-    //   if (session?.user.name && trigger === "update") {
-    //     token.name = session.user.name;
-    //   }
+        //       // Assign new cart
+        //       await prisma.cart.update({
+        //         where: { id: sessionCart.id },
+        //         data: { userId: user.id },
+        //       });
+        //     }
+        //   }
+        // }
+      }
 
-    //   return token;
-    // },
+      // Handle session updates
+      // if (session?.user.name && trigger === "update") {
+      //   token.name = session.user.name;
+      // }
+
+      return token;
+    },
   },
 };
 
